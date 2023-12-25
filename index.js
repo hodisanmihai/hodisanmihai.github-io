4bb665a8e19f3dfd3f5a47c8f2ad61764b85756b
hodisan mihai <mihaihodisan@gmail.com>
2023-12-25 05:52:26 +0200

    update

--- index.js
+++ index.js
@@ -1,168 +1,21 @@
-let activePage = "home";
+var activePage = "projects";
 
-(function () {
-  const hash = window.location.hash.substring(1);
-
-  if (hash) {
-    const page = document.querySelector("#" + hash);
-    if (page && page.classList.contains("page")) {
-      activePage = hash;
-    }
-  }
-})();
+function show(id) {
+  console.info("show", id);
+  var page = document.getElementById(id);
+  console.info("page", page);
+  page.style.display = "block";
+}
 
 function hide(id) {
-  const el = document.getElementById(id);
-  if (el) {
-    el.style.display = "none";
-  } else {
-    console.error(
-      "element you are searching does not exist... check your selector"
-    );
-  }
+  console.info("hide", id);
+  document.getElementById(id).style.display = "none";
 }
 
-function hidePreviousPage() {
+function showPage(id) {
+  console.info("show page", id);
   hide(activePage);
-  const link = document.querySelector(
-    `#top-menu-bar a[data-page="${activePage}"]`
-  );
-  link.classList.remove("active");
+  show(id);
+  activePage = id;
 }
-
-function showPage(pageId) {
-  hidePreviousPage();
-  document.getElementById(pageId).style.display = "";
-  const link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
-  link.classList.add("active");
-  activePage = pageId;
-}
-
-function initMenu() {
-  document.addEventListener("click", (e) => {
-    const link = e.target;
-    if (link.matches("#top-menu-bar a")) {
-      const id = link.getAttribute("data-page");
-      showPage(id);
-    }
-  });
-
-  document.querySelector("#colorblind").addEventListener("click", () => {
-    document.body.classList.toggle("grayscale");
-  });
-}
-
-initMenu();
-
-showPage(activePage);
-
-function getHTMLSkills(skills) {
-  return skills
-    .map(
-      (skill) =>
-        `<li class="${skill.endorsements > 9 ? "favorite" : ""}">
-            ${skill.name} <span>&middot; ${skill.endorsements}</span>
-        </li>`
-    )
-    .join("");
-}
-
-function showSkills(skills) {
-  const ul = document.querySelector("#skills ul");
-  ul.innerHTML = getHTMLSkills(skills);
-}
-
-fetch("/data/skills.json")
-  .then((r) => r.json())
-  .then((allSkills) => {
-    allSkills.sort((s1, s2) => s2.endorsements - s1.endorsements);
-
-    showSkills(allSkills);
-  });
-
-setTimeout(() => {
-  initRubik(document.getElementById("rubikChallenge"));
-  initRubik(document.getElementById("rubikChallengePage"));
-}, 10);
-
-let activePage = "home";
-
-(function () {
-  const hash = window.location.hash.substring(1);
-
-  if (hash) {
-    const page = document.querySelector("#" + hash);
-    if (page && page.classList.contains("page")) {
-      activePage = hash;
-    }
-  }
-})();
-
-function hide(id) {
-  const el = document.getElementById(id);
-  if (el) {
-    el.style.display = "none";
-  } else {
-    console.error(
-      "element you are searching does not exist... check your selector"
-    );
-  }
-}
-
-function hidePreviousPage() {
-  hide(activePage);
-  const link = document.querySelector(
-    `#top-menu-bar a[data-page="${activePage}"]`
-  );
-  link.classList.remove("active");
-}
-
-function showPage(pageId) {
-  hidePreviousPage();
-  document.getElementById(pageId).style.display = "";
-  const link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
-  link.classList.add("active");
-  activePage = pageId;
-}
-
-function initMenu() {
-  document.addEventListener("click", (e) => {
-    const link = e.target;
-    if (link.matches("#top-menu-bar a")) {
-      const id = link.getAttribute("data-page");
-      showPage(id);
-    }
-  });
-
-  document.querySelector("#colorblind").addEventListener("click", () => {
-    document.body.classList.toggle("grayscale");
-  });
-}
-
-initMenu();
-
-showPage(activePage);
-
-function getHTMLSkills(skills) {
-  return skills
-    .map(
-      (skill) =>
-        `<li class="${skill.endorsements > 9 ? "favorite" : ""}">
-            ${skill.name} <span>&middot; ${skill.endorsements}</span>
-        </li>`
-    )
-    .join("");
-}
-
-function showSkills(skills) {
-  const ul = document.querySelector("#skills ul");
-  ul.innerHTML = getHTMLSkills(skills);
-}
-
-fetch("/data/skills.json")
-  .then((r) => r.json())
-  .then((allSkills) => {
-    allSkills.sort((s1, s2) => s2.endorsements - s1.endorsements);
-
-    showSkills(allSkills);
-  });
+show(activePage);
