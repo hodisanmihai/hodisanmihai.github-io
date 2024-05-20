424fd84fd70b2776dd1b867b63cb253167e70af2
hodisan mihai <mihaihodisan@gmail.com>
2024-05-20 21:22:11 +0300

    update

--- index.js
+++ index.js
@@ -1,26 +1,83 @@
-var activePage = "home";
+// variable publice
+var activePage = "skills";
 
-function show(id) {
-  console.info("show", id);
-  var page = document.getElementById(id);
-  console.info("page", page);
-  page.style.display = "block";
-}
-
+// functii publice
 function hide(id) {
   console.info("hide", id);
   document.getElementById(id).style.display = "none";
 }
 
+function show(id) {
+  console.info("show", id);
+  var page = document.getElementById(id);
+  console.debug("show page", page);
+  page.style.display = "block";
+}
+
 function showPage(id) {
   console.info("show page", id);
+  var prevLink = document.querySelector("a[data-page=" + activePage + "]");
+  prevLink.classList.remove("active");
   hide(activePage);
+
+  var nextLink = document.querySelector(`a[data-page=${id}]`);
+  nextLink.classList.add("active");
   show(id);
   activePage = id;
 }
 
+function initEvents() {
+  var toolbar = document.querySelector("#top-menu-bar");
+  toolbar.addEventListener("click", function (e) {
+    if (e.target.matches("a")) {
+      var page = e.target.dataset.page;
+      console.warn("click", page);
+      showPage(page);
+    }
+  });
+}
+
 function showSkills() {
-  var ul = document.querySelector("skills ul");
-  ul.innerHTML = "<li>HTMl</li>";
-  ul.innerHTML = "<li>CSS</li>";
-  ul.innerHTML = "<li>JS</li>";
+  var ul = document.querySelector("#skills ul");
+
+  var skills = [
+    {
+      name: "HTML",
+      endorcements: 6,
+      favorite: true,
+    },
+    {
+      name: "CSS",
+      endorcements: 5,
+    },
+    {
+      name: "JS",
+      endorcements: 7,
+      favorite: true,
+    },
+    {
+      name: "Word",
+      endorcements: 1,
+      favorite: false,
+    },
+  ];
+
+  var text = skills.map(function (skill) {
+    var cls = "";
+    if (skill.favorite == true) {
+      cls = "favorite";
+    }
+
+    console.info("%o (%o)", skill.name, cls);
+    return `<li class="${cls}">${skill.name} <span> - ${skill.endorcements}</span></li>`;
+  });
+  console.warn(text);
+
+  ul.innerHTML = text.join("");
+}
+
+// excutii
+showSkills();
+showPage(activePage);
+initEvents();
+  
